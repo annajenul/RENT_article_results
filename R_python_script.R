aa16e19d637fc4277bedca48d71c8257d1a62482
annajenul <anna.jenul@nmbu.no>
2021-09-25 15:55:50 +0200

    Update

---
+++ R_python_script.R
@@ -0,0 +1,75 @@
+library(reticulate)
+
+# import modules
+pd <- import("pandas")
+np <- import("numpy")
+skl_lr <- import("sklearn.linear_model")
+skl_prepro <- import("sklearn.preprocessing")
+metrics = import("sklearn.metrics")
+
+classification_scores <- function(train_data, train_labels, 
+                                  test_data, test_labels){
+  
+  if (!is.data.frame(train_data)) {
+    train_data = as.matrix(train_data)
+    test_data = as.matrix(test_data)
+  }
+  
+  # convert to python objects
+  py_train <- r_to_py(train_data)
+  py_train_labels <- r_to_py(train_labels)
+  py_test <- r_to_py(test_data)
+  py_test_labels <- r_to_py(test_labels)
+  
+  # standardize
+  sc = skl_prepro$StandardScaler()
+  py_train_sc = sc$fit_transform(py_train)
+  py_test_sc = sc$transform(py_test)
+  
+  #Logreg model
+  model = skl_lr$LogisticRegression(penalty="none",max_iter=8000, solver="saga")#, random_state= np$random$RandomState(0)) 
+  model$fit(py_train_sc, py_train_labels)
+  
+  # predict
+  f1_1 = metrics$f1_score(py_test_labels, model$predict(py_test_sc), pos_label=1)
+  f1_0 = metrics$f1_score(py_test_labels, model$predict(py_test_sc), pos_label=0)
+  acc = metrics$accuracy_score(py_test_labels, model$predict(py_test_sc))
+  mcc = metrics$matthews_corrcoef(py_test_labels, model$predict(py_test_sc))
+  
+  results = list("f1_1"=f1_1, "f1_0"=f1_0, "acc" = acc, "mcc" = mcc)
+  return(results)
+}
+
+
+regression_scores <- function(train_data, train_labels, 
+                                  test_data, test_labels){
+  
+  if (!is.data.frame(train_data)) {
+    train_data = as.matrix(train_data)
+    test_data = as.matrix(test_data)
+  }
+  
+  # convert to python objects
+  py_train <<- r_to_py(train_data)
+  py_train_labels <- r_to_py(train_labels)
+  py_test <- r_to_py(test_data)
+  py_test_labels <- r_to_py(test_labels)
+  
+  # standardize
+  sc = skl_prepro$StandardScaler()
+  py_train_sc = sc$fit_transform(py_train)
+  py_test_sc = sc$transform(py_test)
+  
+  
+  # Linear Model
+  model = skl_lr$LinearRegression() 
+  model$fit(py_train_sc, py_train_labels)
+  
+  # predict
+  rmse = np$sqrt(metrics$mean_squared_error(py_test_labels, model$predict(py_test_sc)))
+  r2 = metrics$r2_score(py_test_labels, model$predict(py_test_sc))
+  
+  results = list("rmse"=rmse, "r2"=r2)
+  return(results)
+}
+
---
+++ stabsel_gridsearch.R
@@ -0,0 +1,98 @@
+library(stabs)
+library(glmnet)
+library(mltools)
+library(caret)
+
+
+stabsel_gridsearch <- function(train_data, train_labels, B=100){
+  set.seed(0)
+  PFER_values = seq(0.05,0.95,0.05)
+  cutoff_values = seq(0.6,0.9,0.05)
+  folds <- createFolds(train_labels, k=5, list=TRUE)
+  
+  
+  mcc_average_scores = matrix(nrow=length(PFER_values), ncol = length(cutoff_values))
+  
+  rownames(mcc_average_scores)=PFER_values
+  colnames(mcc_average_scores)=cutoff_values
+  for (i in 1:length(PFER_values)) {
+    for (j in 1:length(cutoff_values)) {
+      cat("pfer:",PFER_values[i], "cutoff:", cutoff_values[j], "\n")
+      average_performance = c()
+      for (fold in folds) {
+
+        stab.glmnet = stabsel(x = apply(train_data[-fold,], 2, function(x){if(length(unique(x))>1){return(scale(x))} else{return(x)}}), 
+                              y = train_labels[-fold],
+                              fitfun = glmnet.lasso,
+                              args.fitfun = list(family = "binomial"),
+                              cutoff = cutoff_values[j],
+                              PFER = PFER_values[i],
+                              B = B,
+                              sampling.type="MB")
+        
+        selected_features = stab.glmnet$selected
+        cat("LENGHT SELECTED FEATURES:", length(selected_features), "\n")
+        
+        if (length(selected_features) == 0) {
+          average_performance = c(average_performance, NA)
+        }
+        else{
+          
+          print("here")
+          # standardize data
+          mean_train = apply(train_data[-fold,], 2, mean)
+          std_train = apply(train_data[-fold,], 2, sd)
+          train_data_sc = as.matrix(sweep(
+            sweep(train_data[-fold,],2, mean_train),
+            2, std_train, "/"))
+          
+          
+          test_data_sc = as.matrix(sweep(
+            sweep(train_data[fold,],2, mean_train),
+            2, std_train, "/"))
+          
+          
+          train_data_sc = train_data_sc[,names(selected_features)]
+          test_data_sc = test_data_sc[,names(selected_features)]
+          
+          if (is.vector(train_data_sc)) {
+            train_data_sc = data.frame(train_data_sc)
+            colnames(train_data_sc) = names(selected_features)
+          }
+          
+          glm_data = data.frame(cbind("y"=train_labels[-fold], train_data_sc))
+          glm.fit <- glm(y~., data=glm_data, family = binomial)
+          pred_data = data.frame(test_data_sc)
+          colnames(pred_data) = colnames(train_data_sc)
+          glm.pred = 1*(predict(glm.fit, newdata = pred_data,
+                                type="response") >= 0.5)
+          
+          average_performance = c(average_performance, 
+                                  mltools::mcc(preds=glm.pred, actual=train_labels[fold]))
+        }
+        if (all(is.na(average_performance))) {
+          mcc_average_scores[i,j] = NA
+        }
+        mcc_average_scores[i,j] = mean(average_performance, na.rm=TRUE)
+        
+      }
+    }
+    
+  }
+  
+  print(mcc_average_scores)
+  
+  params = which(mcc_average_scores == max(mcc_average_scores, na.rm=TRUE), arr.ind = TRUE)
+  pfer = PFER_values[min(params[,1])]
+  cutoff = cutoff_values[max(params[params[,1]==min(params[,1])][-1])]
+    
+  stab.glmnet = stabsel(x = apply(train_data, 2, function(x){if(length(unique(x))>1){return(scale(x))} else{return(x)}}), 
+                        y = train_labels,
+                        fitfun = glmnet.lasso,
+                        args.fitfun = list(family = "binomial"),
+                        cutoff = cutoff,
+                        PFER = pfer,
+                        B = B)
+  
+  return(stab.glmnet$selected)
+}
